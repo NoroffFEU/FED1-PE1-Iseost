1c509829ed552f13181b41d3c9f3ce847e9a7f4f
Iseost <iselin.osterhus@gmail.com>
2024-05-24 16:15:38 +0200

    post index
    post index is updated with author, created date and update date. also finnishs styled

--- js/index.mjs
+++ js/index.mjs
@@ -25,14 +25,28 @@ async function get_blogpost(id) {
         const title = tech.data.title;
         const body = tech.data.body;
         const media = tech.data.media;
+        const author = tech.data.author;
+        const created = tech.data.created;
+        const updated = tech.data.updated;
+        const share = tech.data.id;
         const titlebox = document.getElementById('title')
         const blogbox = document.getElementById('content')
         const imagebox = document.getElementById('image')
+        const authorbox = document.getElementById('author')
+        const createdbox = document.getElementById('created')
+        const updatedbox = document.getElementById('updated')
+        const sharebox = document.getElementById('share')
         const editbutton = document.getElementById('edit_button')
+       
         titlebox.innerHTML = title;
         blogbox.innerHTML = body;
         imagebox.src = media.url;
         imagebox.setAttribute('alt', media.alt);
+
+        authorbox.innerHTML = `Author: ${author.name}`;
+        createdbox.innerHTML = `Created: ${created}`;
+        updatedbox.innerHTML = `Updated: ${updated}`;
+        sharebox.innerHTML = `Share this post: ${share}`;
         editbutton.href = `/post/edit.html?id=${id}` 
     }
 }
