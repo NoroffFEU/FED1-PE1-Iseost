b4abecbbe04a220d2b2e374cb7021526554c8139
Iseost <iselin.osterhus@gmail.com>
2024-05-24 21:45:00 +0200

    footer
    footer is made and finnish

---
+++ js/footer.mjs
@@ -0,0 +1,19 @@
+
+
+const appFooter = `
+    <div class="flex logo_text_footer">
+    <img class="logo rounded" src="./assets/6502423.jpg" alt="Logo" />
+    <p class="text_header">HotView Labs</p>
+  </div>
+  <div class="flex address">
+    <h2 class="title_address">Address</h2>
+    <p>
+      HotView Labs Level 5, Suite 327 Imperial Trade Center Coruscant,
+      Galactic City 110011 Sector 001, Core Worlds Galactic Republic
+    </p>
+  </div>
+    `;
+document.getElementById("app-footer").innerHTML = appFooter;
+
+
+
